806cab4e266277f3206a851cdb0d82e7f1ba6ed8
febianbatubara <febianbatubaraa@gmail.com>
2021-03-26 23:38:15 +0700

    add FavoriteActivity view

---
+++ app/src/main/java/com/febian/android/githubmates/activity/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.febian.android.githubmates.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.febian.android.githubmates.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
