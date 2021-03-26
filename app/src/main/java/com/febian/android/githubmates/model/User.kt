aede91bfc0afd384fdaeda7ec0bb3e8e1ef05365
febianbatubara <febianbatubaraa@gmail.com>
2021-03-27 00:01:48 +0700

    annotating user model for Room

--- app/src/main/java/com/febian/android/githubmates/model/User.kt
+++ app/src/main/java/com/febian/android/githubmates/model/User.kt
@@ -1,12 +1,16 @@
 package com.febian.android.githubmates.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
+@Entity
 @Parcelize
 data class User(
+    @PrimaryKey
     @SerializedName("login")
     @Expose
     val username: String?,
