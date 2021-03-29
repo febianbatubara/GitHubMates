79c2439f8d4c37344a64fda7d135ba214b94e549
febianbatubara <febianbatubaraa@gmail.com>
2021-03-30 03:05:17 +0700

    add stack widget favorite user

---
+++ app/src/main/java/com/febian/android/githubmates/widget/FavoriteUserWidget.kt
@@ -0,0 +1,69 @@
+package com.febian.android.githubmates.widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.RemoteViews
+import android.widget.Toast
+import com.febian.android.githubmates.R
+
+class FavoriteUserWidget : AppWidgetProvider() {
+
+    companion object {
+        private const val TOAST_ACTION = "com.febian.android.githubmates.TOAST_ACTION"
+        const val EXTRA_ITEM = "com.febian.android.githubmates.EXTRA_ITEM"
+
+        fun updateAppWidget(
+            context: Context,
+            appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+            val intent = Intent(context, StackWidgetService::class.java)
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME))
+
+            val views = RemoteViews(context.packageName, R.layout.favorite_user_widget)
+            views.setRemoteAdapter(R.id.stack_view, intent)
+            views.setEmptyView(R.id.stack_view, R.id.empty_view)
+
+            val toastIntent = Intent(context, FavoriteUserWidget::class.java)
+            toastIntent.action = TOAST_ACTION
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+            intent.data = Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME))
+            val toastPendingIntent =
+                PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    toastIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+            views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent)
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if (intent.action != null) {
+            if (intent.action == TOAST_ACTION) {
+                val viewIndex = intent.getIntExtra(EXTRA_ITEM, 0)
+                Toast.makeText(context, "Favorite User ${viewIndex + 1}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
